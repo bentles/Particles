bf06f5d4b81a2a4e3b6bf7a8df4ee6ae752435c4
bentles <dee.bent.lee@gmail.com>
2021-11-16 23:59:16 +0200

    this is now very very slow

--- Particle/Particle.gd
+++ Particle/Particle.gd
@@ -7,11 +7,12 @@ export var fmass = 1
 # maybe can make these asymmetric later if it makes things cooler
 const cooldown_seconds = 0.5
 var cooldown_elapsed_seconds = cooldown_seconds
-const action_seconds = 0.2
+const action_seconds = 0.5
 var action_elapsed_seconds = action_seconds
 
-const State = { STATE_IDLE = 0, STATE_EXPANDING = 1, STATE_EXPANDED = 3, 
-	STATE_CONTRACTING = 3}
+const size_factor = 0.5;
+
+const State = { STATE_IDLE = 0, STATE_EXPANDING = 1, STATE_EXPANDED = 3, STATE_CONTRACTING = 2}
 
 var state = State.STATE_IDLE
 
@@ -20,6 +21,24 @@ func _act(from_state, to_state):
 		state = to_state
 		action_elapsed_seconds = 0
 		cooldown_elapsed_seconds = 0
+		
+func _resize_model():
+	var percent = action_elapsed_seconds / action_seconds
+	var size_state = 0
+	
+	if state == State.STATE_EXPANDED:
+		size_state = 1
+	elif state == State.STATE_IDLE:
+		size_state = 0
+	elif state == State.STATE_CONTRACTING:
+		size_state = (1 - percent)
+	elif state == State.STATE_EXPANDING:
+		size_state = percent
+		
+	var size = 1 + size_factor * size_state
+	self.scale.x = size;
+	self.scale.y = size;
+	self.scale.z = size;
 
 func _expand():
 	_act(State.STATE_IDLE, State.STATE_EXPANDING)
@@ -27,28 +46,34 @@ func _expand():
 func _contract():
 	_act(State.STATE_EXPANDED, State.STATE_CONTRACTING)
 
+var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 	
 func _process_state(delta):
 	# TODO: might need to think about order here
 	if state == State.STATE_EXPANDING || state == State.STATE_CONTRACTING:
 		action_elapsed_seconds += delta
 		if action_elapsed_seconds >= action_seconds:
-			state = state.STATE_IDLE if state == State.STATE_CONTRACTING else State.STATE_EXPANDED
-			return
+			action_elapsed_seconds = action_seconds
+			state = State.STATE_IDLE if state == State.STATE_CONTRACTING else State.STATE_EXPANDED
+		return
 	if (state == State.STATE_IDLE || state == State.STATE_EXPANDED) && \
 	cooldown_elapsed_seconds < cooldown_seconds:
 		cooldown_elapsed_seconds += delta
 
-
+var rand_time = 0;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	_process_state(delta)
+	
+	_resize_model()
 	# assume for now all particles are the same mass so i just pick a
 	# value for G*m1*m2
+	
+	
 	var gmm = 5.0
 	
 	var overlapping = $InfluenceArea.get_overlapping_bodies()	
--- Particle/ParticleBrain.gd
+++ Particle/ParticleBrain.gd
@@ -1,10 +1,17 @@
 const NeuralNetwork = preload("../Neural Network/Brain.gd")
 
-var neural_network = NeuralNetwork.new(2,3,4)
+
+var neural_network = NeuralNetwork.new(2,5,3)
 
 # ok i think each particle should know about the particles next to it
 # so maybe direction vectors to the particles in its sphere??
 # definitely should know the state of the particles next to it
+
+# 3 outputs: expand, contract, do nothing
+# inputs: what particles around it are doing
+# how on earth do I format this??
+
+# some number of middle nodes
 
 # states:
 # 0: rest, 1: expanding, 2: expanded, 3:contracting
